dec33e683f701a4978bdcd1ec79690c2e6f424ae
Ahsan Inam <ceo@xsol.ai>
2024-11-25 02:18:27 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -44,4 +44,4 @@ app.include_router(history.router)
 app.include_router(auth.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
