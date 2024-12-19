c86ab8ff11b32e9712549387c1180d299c476e88
Ahsan Inam <ceo@xsol.ai>
2024-12-20 02:59:10 +0500

    Update db_connection.py

--- app/database/db/db_connection.py
+++ app/database/db/db_connection.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 # Create SQLite engine and session
 DATABASE_URL = "sqlite:///./guru_call_data.db"
 # DATABASE_URL = "sqlite:///./example.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False},pool_size=20, max_overflow=0)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False},pool_size=50, max_overflow=0)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
