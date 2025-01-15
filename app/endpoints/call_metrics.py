a6b91559c48f77051563ac23119a9ba270e94284
Saba Saeed <sabasaeed@xsol.ai>
2025-01-15 14:49:13 +0500

    updated backend

--- app/endpoints/call_metrics.py
+++ app/endpoints/call_metrics.py
@@ -241,15 +241,9 @@ async def get_calls(
                 },
                 "Time metrics":{
                     "weekday": row.weekday,
-<<<<<<< Updated upstream
-                    "avg_wait_time_sec": round((row.avg_wait_time) or 0, 2),
-                    "max_wait_time_sec": round((row.max_wait_time) or 0, 2),
-                    "avg_handling_time": round((row.avg_handling_time) or 0, 2),
-=======
                     "avg_wait_time_sec": round((row.avg_wait_time)/60 or 0, 2),
                     "max_wait_time_sec": round((row.max_wait_time)/60 or 0, 2),
                     "avg_handling_time": round((row.avg_handling_time)/60 or 0, 2),
->>>>>>> Stashed changes
                 },
                 "% metrics":{
                     "weekday": row.weekday,
@@ -262,17 +256,10 @@ async def get_calls(
         "total_call_reasons": total_call_reasons, 
         "asr": round(asr, 2),
         "SLA":round(get_sla_percentage(query, start_date=start_date, end_date=end_date), 2),
-<<<<<<< Updated upstream
-        "avg wait time": round(get_average_wait_time(query, start_date=start_date, end_date=end_date), 2),
-        "max. wait time": round(get_max_wait_time(query, start_date=start_date, end_date=end_date), 2),
-        "After call work time": round(get_inbound_after_call(query, start_date=start_date, end_date=end_date), 2),
-        "avg handling time": round((avg_handling_time) or 0, 2),
-=======
         "avg wait time (min)": round(get_average_wait_time(query, start_date=start_date, end_date=end_date) / 60, 2),
         "max. wait time (min)": round(get_max_wait_time(query, start_date=start_date, end_date=end_date) / 60, 2),
         "After call work time (min)": round(get_inbound_after_call(query, start_date=start_date, end_date=end_date) / 60, 2),
         "avg handling time (min)": round((avg_handling_time or 0) / 60, 2),
->>>>>>> Stashed changes
         "Dropped calls": int(dropped_calls or 0),
         "Daily Call Volume": result  
     }
